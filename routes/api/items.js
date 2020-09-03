19ad3538fd54aa1de006d1cd801240a269a3cf0c
jameskomeara <jamesomeara2000@gmail.com>
2020-09-04 00:05:41 +0100

    added continous user auth

--- routes/api/items.js
+++ routes/api/items.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 
 // @route POST api/items
 // @desc Create An Item
-// @access Public
+// @access Private
 router.post('/', auth, (req, res) => {
     const newItem = new Item({
         name: req.body.name
@@ -28,7 +28,7 @@ router.post('/', auth, (req, res) => {
 
 // @route DELETE api/items/:id
 // @descDelete An Item
-// @access Public
+// @access Private
 router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({success:true})))
