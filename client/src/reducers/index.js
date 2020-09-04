9e1c7f7e4a2be239cbe7c9183f7f97036745d590
jameskomeara <jamesomeara2000@gmail.com>
2020-09-04 20:16:41 +0100

    Added login routes and actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,13 @@
 import {combineReducers} from 'redux'
 import itemReducer from './itemReducer.js'
+import authReducer from './authReducer'
+import errorReducer from './errorReducer'
+
 
 // Combines all the reducers into one so that they do not all have to be exported
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer
+
 })
